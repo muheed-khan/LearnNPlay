926b22697dc406aa184c8da3a16f4c1798a6bb8d
Muheed Khan <muheedk999@gmail.com>
2023-12-30 21:20:34 +0500

    Java files for Home and Main

---
+++ app/src/main/java/com/example/learnnplay/HomePage.java
@@ -0,0 +1,16 @@
+package com.example.learnnplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.learnnplay.R;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+    }
+}
--- app/src/main/java/com/example/learnnplay/MainActivity.java
+++ app/src/main/java/com/example/learnnplay/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.learnnplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String email = "muheedk999@gmail.com";
+    private String pass = "muheed123";
+    EditText mail;
+    EditText password;
+    Button btn;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login);
+        mail = (EditText) findViewById(R.id.emailEditText);
+        password = (EditText) findViewById(R.id.passwordEditText);
+        btn = (Button) findViewById(R.id.loginSignupButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strMail = mail.getText().toString();
+                String strPass = password.getText().toString();
+                    if (email == strMail && pass == strPass){
+
+        }
+            }
+        });
+
+
+
+
+
+
     }
 }
