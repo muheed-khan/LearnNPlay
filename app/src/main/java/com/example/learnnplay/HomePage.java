6c2c88d850567501fea95e161b69577088e8b5e9
Muheed Khan <muheedk999@gmail.com>
2023-12-31 14:35:57 +0500

    login and home page Front-end connection build

--- app/src/main/java/com/example/learnnplay/HomePage.java
+++ app/src/main/java/com/example/learnnplay/HomePage.java
@@ -3,14 +3,31 @@ package com.example.learnnplay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.example.learnnplay.R;
 
 public class HomePage extends AppCompatActivity {
+    Button alpha;
+    Button shape;
+    Button color;
+    Button progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+        alpha = (Button) findViewById(R.id.alphabetsButton);
+        shape = (Button) findViewById(R.id.shapesButton);
+        color =(Button) findViewById(R.id.colorsButton);
+        progress = (Button) findViewById(R.id.progressFeedbackButton);
+        alpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/learnnplay/MainActivity.java
+++ app/src/main/java/com/example/learnnplay/MainActivity.java
@@ -2,35 +2,40 @@ package com.example.learnnplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+//import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String email = "muheedk999@gmail.com";
-    private String pass = "muheed123";
+    private String email = "admin";
+    private String pass = "admin";
     EditText mail;
     EditText password;
     Button btn;
-    @SuppressLint("MissingInflatedId")
+//    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        mail = (EditText) findViewById(R.id.emailEditText);
-        password = (EditText) findViewById(R.id.passwordEditText);
-        btn = (Button) findViewById(R.id.loginSignupButton);
+        mail = (EditText) findViewById(R.id.mail);
+        password = (EditText) findViewById(R.id.pass);
+        btn = (Button) findViewById(R.id.loginBtn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String strMail = mail.getText().toString();
-                String strPass = password.getText().toString();
-                    if (email == strMail && pass == strPass){
+//                Toast.makeText(getApplicationContext(),"Email : " + (mail.getText().toString()).getClass().getSimpleName()+ " Pass : " + password.getClass().getSimpleName(),Toast.LENGTH_LONG).show();
+                if (mail.getText().toString().equals(email) && password.getText().toString().equals(pass)) {
+                    Intent intent = new Intent(MainActivity.this, HomePage.class);
+                    startActivity(intent);
+//                    Log.d("MyApp", "after statement");
+                }
 
-        }
             }
         });
 
