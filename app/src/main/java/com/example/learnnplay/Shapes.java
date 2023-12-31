87b0053bce391fff882303322cac728fa1673124
Huzaifa Ali Baig <huzaifa.ali.baig@hotmail.com>
2023-12-31 15:48:31 +0500

    Created the Rest of  Activity

---
+++ app/src/main/java/com/example/learnnplay/Shapes.java
@@ -0,0 +1,13 @@
+package com.example.learnnplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Shapes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shapes);
+    }
+}
