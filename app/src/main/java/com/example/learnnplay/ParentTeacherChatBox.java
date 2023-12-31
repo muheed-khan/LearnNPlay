0add9edcc791708e121239016f9a61f9d0c3f277
Muheed Khan <muheedk999@gmail.com>
2023-12-31 20:46:37 +0500

    done from my side

---
+++ app/src/main/java/com/example/learnnplay/ParentTeacherChatBox.java
@@ -0,0 +1,14 @@
+package com.example.learnnplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParentTeacherChatBox extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_teacher_chat_box);
+    }
+}
