e6a587af7b92bec07f30f7f4e6af04003039672f
Muheed Khan <muheedk999@gmail.com>
2023-12-31 18:50:30 +0500

    Home Page connection with all game activities

--- app/src/main/java/com/example/learnnplay/parentFeedBack.java
+++ app/src/main/java/com/example/learnnplay/parentFeedBack.java
@@ -1,2 +1,13 @@
-package com.example.learnnplay;public class parentFeedBack {
+package com.example.learnnplay;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class parentFeedBack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_login);
+    }
 }
